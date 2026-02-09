684454c3ccc73d310a887b576b3d45cad9ae8006
wonhyoungpark1983-hash <wonhyoung.park1983@gmail.com>
2026-02-09 11:31:34 +0900

    chore: release v1.5.0 stable

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1244,7 +1244,7 @@ const AdminDashboard = () => {
                 </div>
             )}
             <div className="mt-8 text-center text-xs text-gray-400 opacity-50 pb-4">
-                v1.4.7 (UI Clean)
+                v1.5.0
             </div>
         </div>
     );
